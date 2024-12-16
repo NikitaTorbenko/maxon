79d9e606e25a69647da0339b4923622ecd4b2c1f
NikitaTorbenko <2015nekitciti@gmail.com>
2024-12-16 10:37:51 +0300

    capacitor init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    outDir: "dist", // Папка сборки
+  },
+  server: {
+    host: true, // Доступ по локальной сети
+    port: 3000,
+  },
+});
